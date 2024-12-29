bc5f39cc7086cdd918bf5f2562bac0b6ff07a772
Gerald max <166443104+13065994@users.noreply.github.com>
2024-12-29 14:10:34 +0100

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,8 @@ const fs = require("fs-extra");
 const readline = require("readline");
 const totp = require("totp-generator");
 const login = require("helyt");
-login({email: "100068910213508", password: "123456a@"}, (err, api) => {
+const locateEmailPassword = require("./accountLogins.json");
+login({ email: locateEmailPassword.accEmailVerify, password: locateEmailPassword.accPassword }, (err, api) => {
     if(err) return console.error(err);
     const json = JSON.stringify(api.getAppState());
     fs.writeFileSync(`./${config.APPSTATEPATH}`, json);

52dcbbb6a023673ec62765048487b9b90e8fd6da
Gerald max <166443104+13065994@users.noreply.github.com>
2024-12-29 15:34:46 +0100

    Update calluser.js

--- modules/commands/calluser.js
+++ modules/commands/calluser.js
@@ -1,19 +1,25 @@
 module.exports.config = {
-	name: "ping",
-	version: "1.0.5",
+	name: "sendmessage",
+	version: "1.0",
 	hasPermssion: 1,
-	credits: "Mirai Team",
-	description: "tag toàn bộ thành viên",
-	commandCategory: "group",
+	credits: "Gerald",
+	description: "",
+	commandCategory: "",
 	usages: "[Text]",
 	cooldowns: 5
 };
 
 module.exports.run = async function({ api, event, args }) {
-  try {
-    const first = args[1];
-    api.sendMessage(`you are now texting with ${args[0]}.`, event.threadID);
-    const { messageID } = api.sendMessage(`You have a new message,\n- From: ` + event.senderID + `to ` + args[0], args[0]);
-    api.editMessage(first + " use +sendmessage [the Number see in the *from*] to continue chat", messageID);
-  }
+	if (!args[0]) return api.sendMessage(`provide an id`, event.threadID);
+	else if (!args[1]) return message.reply("you didn't provide a message");
+	
+	try {
+		const first = args[1];
+		api.sendMessage(`you are now texting with ${args[0]}.`, event.threadID);
+		const { messageID } = api.sendMessage(`You have a new message,\n- From: ` + event.senderID + `to ` + args[0], args[0]);
+		api.editMessage(first + " use +sendmessage [the Number see in the *from*] to continue chat", messageID);
+	} catch (e) {
+		message.reply("sh.");
+		console.log(e);
+	}
 };

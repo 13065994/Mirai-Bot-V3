8f85ac963f871068e7fcc3c5554a3aec3a01ad4a
Gerald max <166443104+13065994@users.noreply.github.com>
2024-12-29 15:27:36 +0100

    Update calluser.js

--- modules/commands/calluser.js
+++ modules/commands/calluser.js
@@ -1,1 +1,19 @@
+module.exports.config = {
+	name: "ping",
+	version: "1.0.5",
+	hasPermssion: 1,
+	credits: "Mirai Team",
+	description: "tag toàn bộ thành viên",
+	commandCategory: "group",
+	usages: "[Text]",
+	cooldowns: 5
+};
 
+module.exports.run = async function({ api, event, args }) {
+  try {
+    const first = args[1];
+    api.sendMessage(`you are now texting with ${args[0]}.`, event.threadID);
+    const { messageID } = api.sendMessage(`You have a new message,\n- From: ` + event.senderID + `to ` + args[0], args[0]);
+    api.editMessage(first + " use +sendmessage [the Number see in the *from*] to continue chat", messageID);
+  }
+};
